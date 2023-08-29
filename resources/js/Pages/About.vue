18410ca5a54efbee65e2ea5dcce2d712828f2781
VenuxAK <anastasioak327@gmail.com>
2023-08-30 00:21:28 +0630

    Setup Vue with Inertia and Vite

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-medium text-center">
+            About
+            <span>
+                <i class="fa-solid fa-address-card"></i>
+            </span>
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-medium text-center">
+            Home
+            <span>
+                <i class="fa-solid fa-home"></i>
+            </span>
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
